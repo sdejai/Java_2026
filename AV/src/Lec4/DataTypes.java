bc7506503a0c76e5f36f048355d8558bedb96308
sdejai <sde.jai.java@gmail.com>
2026-01-13 14:40:40 +0530

    Data type

---
+++ AV/src/Lec4/DataTypes.java
@@ -0,0 +1,9 @@
+package Lec4;
+
+public class DataTypes {
+	public static void main(String[] args) {
+		byte byteNumber=127;
+//		byte byteNumber=128;
+		System.out.println(byteNumber);
+	}
+}
---
+++ AV/src/Lec4/StaticBlock.java
@@ -0,0 +1,11 @@
+package Lec4;
+
+public class StaticBlock {
+	static {
+		System.out.println("Static Body");
+	}
+	public static void main(String[] args){
+		System.out.println("Main Body");
+	}
+	
+}
