c8041385a1b98d2876bc3caa07df189c414f91fa
sdejai <sde.jai.java@gmail.com>
2026-01-14 11:25:25 +0530

    Primitive Data Types

---
+++ AV/src/Lec5/DataType.java
@@ -0,0 +1,23 @@
+package Lec5;
+
+public class DataType {
+	public static void main(String[] args) {
+		byte byteNumber = 12;
+		short shortNumber=1235;
+		int intNumber=432245;
+		long longNumer =987466536;
+		float floatNumber=242.45f;
+		double doubleNumber=13414.8765d;
+		char charater='d';
+		boolean bool=true;
+		System.out.println(byteNumber);
+		System.out.println(shortNumber);
+		System.out.println(intNumber);
+		System.out.println(longNumer);
+		System.out.println(floatNumber);
+		System.out.println(doubleNumber);
+		System.out.println(charater);
+		System.out.println(bool);
+		System.out.println(byteNumber +" "+ shortNumber + " "+intNumber+" "+longNumer+" "+floatNumber+" "+doubleNumber+" "+charater+" "+bool);
+	}
+}
