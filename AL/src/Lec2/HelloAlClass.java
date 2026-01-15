38489ab77cc75b060abcfd1eb8afb9196a3286e2
sdejai <sde.jai.java@gmail.com>
2026-01-15 12:48:58 +0530

    Static block

---
+++ AL/src/Lec2/HelloAlClass.java
@@ -0,0 +1,7 @@
+package Lec2;
+
+public class HelloAlClass {
+	public static void main(String[] args) {
+		System.out.println("Hello Class AL!");
+	}
+}
---
+++ AL/src/Lec3/StaticBlocks.java
@@ -0,0 +1,11 @@
+package Lec3;
+
+public class StaticBlocks {
+	
+	public static void main(String[] args) {
+		System.out.println("Main Method");
+	}
+	static {
+		System.out.println("Static Block");
+	}
+}
