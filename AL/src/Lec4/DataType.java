699d4b98962ac470f0fe59001dea0681836c8eb3
sdejai <sde.jai.java@gmail.com>
2026-01-16 12:52:57 +0530

    Data Type

---
+++ AL/src/Lec4/DataType.java
@@ -0,0 +1,18 @@
+package Lec4;
+
+public class DataType {
+	public static void main(String[] args) {
+		byte byteNumber = 127;
+		System.out.println(byteNumber);
+		short shortNumber = 26578;
+		System.out.println(shortNumber);
+		int intNumber = 1671642;
+		System.out.println(intNumber);
+		long longNUmber=1372822;
+		System.out.println(longNUmber);
+		float floatNumber = 23137.23;
+		System.out.println(floatNumber);
+		double doubleNumber = 132.434d;
+		System.out.println(doubleNumber);
+	}
+}
