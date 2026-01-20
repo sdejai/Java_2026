027a01a70aede01b07c99a288a82ea0c52d8e9dd
sdejai <sde.jai.java@gmail.com>
2026-01-20 13:21:56 +0530

    Data type and Input

---
+++ AL_Lab_1/src/Lec1/DataType.java
@@ -0,0 +1,25 @@
+package Lec1;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		byte byteNumber =12;
+		System.out.println(byteNumber);
+		short shortNumber =13112;
+		System.out.println(shortNumber);
+		int intNumber =14223;
+		System.out.println(intNumber);
+		long longNumber =122245;
+		System.out.println(longNumber);
+		float floatNumber =13423.232f;
+		System.out.println(floatNumber);
+		double doubleNumber =12342.6543d;
+		System.out.println(doubleNumber);
+		char character ='a';
+		System.out.println(character);
+		boolean bool =true;
+		System.out.println(bool);
+		System.out.println(byteNumber+ " "+shortNumber+" " +intNumber +" "+character+" " +bool +" "+longNumber +" "+floatNumber +" "+doubleNumber);
+	}
+
+}
---
+++ AL_Lab_1/src/Lec1/Input.java
@@ -0,0 +1,20 @@
+package Lec1;
+
+import java.util.Scanner;
+
+public class Input {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+////		int num=1984;
+//		int intNumber=sc.nextInt();
+//		System.out.println("User given number is "+intNumber);
+//		long longNumber =sc.nextLong();
+		char charater =sc.nextLine().charAt(0);
+		System.out.println(charater);
+				
+				
+				
+				
+				
+	}
+}
