617eb354312ebc4294335aa7772b3d494e5dd11c
sdejai <sde.jai.java@gmail.com>
2026-01-21 11:04:12 +0530

    Find Greatest Element

---
+++ AV/src/Lec9/GreaterElement.java
@@ -0,0 +1,21 @@
+package Lec9;
+
+import java.util.Scanner;
+
+public class GreaterElement {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num1 =sc.nextInt();
+		int num2=sc.nextInt();
+		int num3=sc.nextInt();
+		if(num1>=num2 && num1>=num3) {
+			System.out.println("Greatest Element is "+num1);
+		}
+		else if(num2>=num1 && num2>=num3) {
+			System.out.println("Greatest Element is "+num2);
+		}
+		else {
+			System.out.println("Greatest Element is "+num3);
+		}
+	}
+}
