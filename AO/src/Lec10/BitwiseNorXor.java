5bd87279644c39bfcc11c00e6ddbc3a33e54f080
sdejai <sde.jai.java@gmail.com>
2026-01-27 22:12:06 +0530

    bitwise and assigment oprators

---
+++ AO/src/Lec10/BitwiseNorXor.java
@@ -0,0 +1,16 @@
+package Lec10;
+
+public class BitwiseNorXor {
+	public static void main(String[] args) {
+		int number = 3;
+		System.out.println(~number);
+		int number1 = -2;
+		System.out.println(~number1);
+		int num1 = 1;
+		int num2 = 2;
+		System.out.println(num1 ^ num2);
+		int number2 = 1;
+		int number3 = 1;
+		System.out.println(number2 ^ number3);
+	}
+}
