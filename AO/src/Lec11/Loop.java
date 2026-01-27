04f714162b12da3a68f6f63496ebd83cb5071fe2
sdejai <sde.jai.java@gmail.com>
2026-01-27 22:12:45 +0530

    for loop

---
+++ AO/src/Lec11/Loop.java
@@ -0,0 +1,26 @@
+package Lec11;
+import java.util.Scanner;
+
+public class Loop {
+
+	public static void main(String[] args) {
+//		for(int st=1 ; st<=10 ; st=st+1) {
+//			System.out.print(st+" ");
+////		}
+//		Scanner sc = new Scanner(System.in);
+//		int number =sc.nextInt();
+//		for(int itr=0;itr<=number;itr=itr+1) {
+//			if(itr%2==0) {
+//				System.out.print(itr+" ");
+//			}
+//		}
+////		for(int itr=0;itr<=number;itr=itr+2) {
+////			System.out.print(itr+" ");
+////		}
+		
+		int num=13457;
+//		1+3+4+5+7
+		System.out.println(num%10);
+	}
+
+}
