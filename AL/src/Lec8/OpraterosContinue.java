9edb7dbbdac4b51218e9b9d93434db3c18bfa453
sdejai <sde.jai.java@gmail.com>
2026-01-27 16:54:05 +0530

    Oprators Bitwise

---
+++ AL/src/Lec8/OpraterosContinue.java
@@ -0,0 +1,36 @@
+package Lec8;
+
+public class OpraterosContinue {
+	public static void main(String[] args) {
+		int num1=150;
+		int  num2=5;
+//		Realtional(Compair) Oprators
+//		System.out.println(num1>num2);//true
+//		System.out.println(num1<num2);//false
+//		System.out.println(num1==num2);//false
+//		System.out.println(num1!=num2);//true
+//		System.out.println(num1>=num2);//true
+//		System.out.println(num1<=num2);//false
+		
+//		Logical 
+//		System.out.println(true && true);//true
+//		System.out.println(false && true);//false
+//		System.out.println(false && false);//false
+//		System.out.println(true && false);//false
+//		System.out.println(true && false);//false
+//		boolean boolean1=true;
+//		boolean boolean2=true;
+//		System.out.println(boolean1 && boolean2);//true
+//		System.out.println(true || true);//true
+//		System.out.println(true || false);//true
+//		System.out.println(false || true);//true
+//		System.out.println(num1>num2 || num1==num2);//true
+//		System.out.println(!true);//false
+		
+//		Bitwise oprators
+//		int number1=5;
+//		int number2=2;
+//		System.out.println(number1&number2);
+		System.out.println(~7);
+	}
+}
