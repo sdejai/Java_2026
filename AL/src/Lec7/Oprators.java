ef22297b6710d83f864c673a96cf0e4969aca96c
sdejai <sde.jai.java@gmail.com>
2026-01-27 16:52:21 +0530

    Oprartos Arithemic

---
+++ AL/src/Lec7/Oprators.java
@@ -0,0 +1,14 @@
+package Lec7;
+
+public class Oprators {
+	public static void main(String[] args) {
+		int num1=12;
+		int num2=3;
+		System.out.println("+ "+(num1+num2));
+		System.out.println("- "+(num1-num2));
+		System.out.println("* "+(num1*num2));
+		System.out.println("* "+(num1/num2));
+		System.out.println("% remender "+(num1%num2));
+		System.out.println(num1>num2);
+	}
+}
