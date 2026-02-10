9e02d1e422b1310241ab0391dc309c9e06aa3e2e
sdejai <sde.jai.java@gmail.com>
2026-02-10 15:29:32 +0530

    Patterns

---
+++ AV/src/Lec18/Patterns.java
@@ -0,0 +1,20 @@
+package Lec18;
+
+import java.util.Scanner;
+
+
+public class Patterns {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int row=0;row<n;row++) {
+			for(int colSpace=0;colSpace<n-1-row;colSpace++) {
+				System.out.print(" ");
+			}
+			for(int colStar=0;colStar<=row;colStar++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
