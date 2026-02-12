22e7e373b19ed6b07dbf77898dea72a1d09d4ce4
sdejai <sde.jai.java@gmail.com>
2026-02-12 13:53:24 +0530

    Hollow Patterns

---
+++ AV/src/Lec19/DowhileLoop.java
@@ -0,0 +1,15 @@
+package Lec19;
+
+public class DowhileLoop {
+	public static void main(String[] args) {
+		int count=10;
+//		while(count<10) {
+//			System.out.print(count+" ");
+//			count++;
+//		}
+		do {
+			System.out.print(count+" ");
+			count++;
+		}while(count<10);
+	}
+}
---
+++ AV/src/Lec19/HollowSqr.java
@@ -0,0 +1,18 @@
+package Lec19;
+
+public class HollowSqr {
+	public static void main(String[] args) {
+		int  n=5;
+		for(int row=0;row<n;row++) {
+			for(int col=0;col<n;col++) {
+				if(row==0 || row==n-1 || col==0 || col==n-1) {
+					System.out.print("*");
+				}	
+				else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
