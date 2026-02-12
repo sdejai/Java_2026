6b4204473a9251a0d9acc76dda889ca35f54a38d
sdejai <sde.jai.java@gmail.com>
2026-02-12 16:10:52 +0530

    do while pascal trangle

---
+++ AL/src/Lec15/DoWhileLoop.java
@@ -0,0 +1,16 @@
+package Lec15;
+
+public class DoWhileLoop {
+	public static void main(String[] args) {
+		int i=10;
+//		while(i<10) {
+//			System.out.print(i+" ");
+//			i++;
+//		}
+		do {
+			System.out.print(i+" ");
+			System.out.println("Hello");
+			i++;
+		}while(i<10);
+	}
+}
---
+++ AL/src/Lec15/PascalTrangle.java
@@ -0,0 +1,24 @@
+package Lec15;
+
+import java.util.Scanner;
+
+public class PascalTrangle {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		int row=0;
+		int star=1;
+		while(row<n) {
+			int col=0;
+			int nCr=1;
+			while(col<star) {
+				System.out.print(nCr+" ");
+				nCr=((row-col)*nCr)/(col+1);
+				col++;
+			}
+			System.out.println();
+			star++;
+			row++;
+		}
+	}
+}
