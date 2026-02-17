40e7e4e5766cb8c16407cbb4bbbe8bcf3262382e
sdejai <sde.jai.java@gmail.com>
2026-02-17 13:20:27 +0530

    Happy Number LeetCode

---
+++ AL/src/Lec20/HappyNumber.java
@@ -0,0 +1,24 @@
+package Lec20;
+
+import java.util.Scanner;
+
+public class HappyNumber {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		while (n!=1 && n!=4) {
+			int sum=0;
+			while (n>0) {
+				int digit = n%10;
+				n=n/10;
+				sum=sum+digit*digit;
+			}
+			n=sum;
+		}
+		if (n==1) {
+			System.out.println("Happy Number");
+		} else {
+			System.out.println("UnHappy");
+		}
+	}
+}
