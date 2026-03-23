2e87ed8dbb88de5f66b64abf39477de7aa13f1fd
sdejai <sde.jai.java@gmail.com>
2026-03-23 15:30:16 +0530

    LeetCode Best Time to Buy Sell

---
+++ AV/src/Lec29/LeetCodeBestTimeStock.java
@@ -0,0 +1,27 @@
+package Lec29;
+
+public class LeetCodeBestTimeStock {
+	public static int bestTimeBuySell(int nums[]) {
+		int maxprofit=0;
+		int buy=nums[0];
+		for(int i=0;i<nums.length;i++) {
+			int sell=nums[i];
+			if(buy>sell) {
+				buy=sell;
+			}
+			int profit=sell-buy;
+			
+//			maxprofit=Math.max(maxprofit,profit);
+			
+			if(profit>maxprofit) {
+				maxprofit=profit;
+			}
+		}
+		
+		return maxprofit;
+	}
+	public static void main(String[] args) {
+		int nums[]= {7,1,5,3,6,4};
+		System.out.println(bestTimeBuySell(nums));
+	}
+}
