2bec20adb4883c9c9fcf4d59959e3f84b3e0a055
sdejai <sde.jai.java@gmail.com>
2026-04-01 15:26:23 +0530

    print chars of String

---
+++ AL/src/LEc33/StringPrint.java
@@ -0,0 +1,14 @@
+package LEc33;
+
+public class StringPrint {
+	public static void main(String[] args) {
+		String name = "Delhi";
+//		for(int i=0;i<name.length();i++) {
+//				System.out.print(name.charAt(i));
+//		}
+		for (int i = name.length()-1; i >=0 ; i--) {
+			System.out.print(name.charAt(i));
+		}
+
+	}
+}
