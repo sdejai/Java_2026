994edf992b6d288f391f9afd97ddbbbddeb35a2e
sdejai <sde.jai.java@gmail.com>
2026-04-01 15:24:43 +0530

    String Basic and Check Palindrom

---
+++ AL/src/Lec32/CheckPalindrom.java
@@ -0,0 +1,20 @@
+package Lec32;
+
+public class CheckPalindrom {
+	public static void main(String[] args) {
+		
+//		String str="aabaaz";
+//		int st=0;
+//		int end=str.length()-1;
+////	    System.out.println(str.length());
+//		while(st<end) {
+//			if(str.charAt(st)!=str.charAt(end)) {
+//				System.out.println("Not Palindrome");
+//				return;
+//			}
+//			st++;
+//			end--;
+//		}
+//		System.out.println("Palindrome");
+	}
+}
---
+++ AL/src/Lec32/StringBasic.java
@@ -0,0 +1,22 @@
+package Lec32;
+
+import java.util.Scanner;
+
+public class StringBasic {
+	public static void main(String[] args) {
+//		String name1="naman";
+//		String name2="naman";
+//		String name3=new String("aman");
+//		String name4=new String("aman");
+//		System.out.println(name1==name2);//true
+//		System.out.println(name3==name4);//false
+//		System.out.println(name3.equals(name4));//true
+//		String name5=new String("naman").intern();
+//		System.out.println(name1==name5);
+		
+		Scanner sc = new Scanner(System.in);
+//		String city =sc.nextLine();
+		String city =sc.next();
+		System.out.println(city);
+	}
+}
